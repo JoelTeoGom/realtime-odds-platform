3b498b86c3dcb59c94983e20fcbe55c61e8bfff1
JoelTeoGom <joel.teodoro.software@gmail.com>
2026-07-28 23:43:26 +0200

    feature(Hub): added hub and shard first implementation to reduce lock contention

--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -1,4 +1,51 @@
 package hub
 
+import (
+	"hash/fnv"
+
+	"github.com/JoelTeoGom/go-sharded-ws-hub/internal/ports/inbound"
+	"github.com/JoelTeoGom/go-sharded-ws-hub/internal/ports/outbound"
+)
+
 type Hub struct {
+	shards  []*Shard
+	nShards int
+}
+
+var _ inbound.Hub = (*Hub)(nil)
+
+func NewHub(nShards int) *Hub {
+	if nShards < 1 {
+		nShards = 1
+	}
+
+	shards := make([]*Shard, nShards)
+	for i := range shards {
+		shards[i] = newShard()
+	}
+
+	return &Hub{
+		nShards: nShards,
+		shards:  shards,
+	}
+}
+
+// shardFor always maps a topic to the same shard.
+func (h *Hub) shardFor(topic string) *Shard {
+	sum := fnv.New32a()
+	_, _ = sum.Write([]byte(topic))
+	return h.shards[sum.Sum32()%uint32(h.nShards)]
+}
+
+func (h *Hub) Subscribe(c outbound.Client, topics ...string) {
+	for _, topic := range topics {
+		h.shardFor(topic).subscribe(c, topic)
+	}
+}
+
+func (h *Hub) Unsubscribe(c outbound.Client, topics ...string) {
+	id := c.ID()
+	for _, topic := range topics {
+		h.shardFor(topic).unsubscribe(id, topic)
+	}
 }
--- internal/hub/shard.go
+++ internal/hub/shard.go
@@ -6,7 +6,41 @@ import (
 	"github.com/JoelTeoGom/go-sharded-ws-hub/internal/ports/outbound"
 )
 
+
 type Shard struct {
-	mu        sync.Mutex
-	clientMap map[string][]*outbound.Client
+	mu   sync.RWMutex
+	subs map[string]map[string]outbound.Client
+}
+
+func newShard() *Shard {
+	return &Shard{
+		subs: make(map[string]map[string]outbound.Client)
+	}
+}
+
+func (s *Shard) subscribe(c outbound.Client, topic string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	clients, ok := s.subs[topic]
+	if !ok {
+		clients = make(map[string]outbound.Client)
+		s.subs[topic] = clients
+	}
+	clients[c.ID()] = c
+}
+
+func (s *Shard) unsubscribe(clientID, topic string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	clients, ok := s.subs[topic]
+	if !ok {
+		return
+	}
+
+	delete(clients, clientID)
+	if len(clients) == 0 {
+		delete(s.subs, topic) // an empty inner map would leak
+	}
 }
